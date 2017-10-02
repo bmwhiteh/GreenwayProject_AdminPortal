953bbf9ae16ef4c7fbad63fad40165bcb6c8e561
Andrea Moorman <andreamoorman26@gmail.com>
2017-10-02 17:57:55 -0400

    Nav Bar updates and dashboard styling

--- htmlPages/dashboard.php
+++ htmlPages/dashboard.php
@@ -6,30 +6,20 @@
     <style>
         body{
             width: 100%;
-            margin: auto;
-            margin: 0 auto;
-            background-color: #AADFEF;
+            overflow: hidden;
+            background-color: #1B371A ;
         }
 
-        .container {
-            padding: 16px;
-            vertical-align: middle;
-            width:35%;
-            margin-top:20%;
-            margin-left:30%;
-            background-color: rgba(211, 211, 211,0.8) ;
-        }
 
-        .header{
-            background-color: rgba(211,211,211,0.8);
-            width:100%;
-            top:0;
-            position:fixed;
-        }
 
         .twitter{
             margin-left:68%;
-            margin-top:2%;
+        }
+
+        .contentBox{
+            background-color: #aaaaaa;
+            margin: 0px 100px 50px 100px;
+
         }
 
     </style>
@@ -37,18 +27,13 @@
 </head>
 
 <body>
-<div class="header">
-    <div class="picLogo">
-        <h1>CoFW Trail Network Admin Portal</h1>
-    </div>
-</div>
-<div class="menu">
-    <?php include "navBar.html"; ?>
-</div>
 
-<div class="twitter">
-    <a class="twitter-timeline" data-width="400" data-height="400" href="https://twitter.com/FortWayneTrails">Tweets by @FortWayneTrails</a>
-    <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+    <?php include "navBar.html"; ?>
+<div class="contentBox">
+    <div class="twitter">
+        <a class="twitter-timeline" data-width="400" data-height="400" href="https://twitter.com/FortWayneTrails">Tweets by @FortWayneTrails</a>
+        <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+    </div>
 </div>
 </body>
 </html>
