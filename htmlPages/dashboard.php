1cb04af683ad4adaa8c2ec759bbd65423c577139
Andrea Moorman <andreamoorman26@gmail.com>
2017-10-03 15:19:21 -0400

    Dashboard: content addition and styling

--- htmlPages/dashboard.php
+++ htmlPages/dashboard.php
@@ -10,16 +10,29 @@
             background-color: #1B371A ;
         }
 
-
-
-        .twitter{
-            margin-left:68%;
-        }
-
         .contentBox{
             background-color: #aaaaaa;
             margin: 0px 100px 50px 100px;
+            display: -webkit-flex;
+            display: flex;
+        }
+        .leftSide{
+            -webkit-flex: 1;
+            -ms-flex: 1;
+            flex: 1;
+        }
+        .heatMap{
+            -webkit-flex: 2;
+            -ms-flex: 2;
+            flex: 2;
+            margin: 3% 1.5% 3% 3%;
+        }
 
+        .twitter{
+            -webkit-flex: 1;
+            -ms-flex: 1;
+            flex: 1;
+            margin: 3% 3% 3% 1.5%
         }
 
     </style>
@@ -30,6 +43,14 @@
 
     <?php include "navBar.html"; ?>
 <div class="contentBox">
+    <div class="leftSide">
+        <div class="topBox" style="background-color: #bbb"> <p>TOP</p>       </div>
+        <div class="midBox" style="background-color: #bbb"><p>MID</p></div>
+        <div class="bottomBox" style="background-color: #bbb"><p>BOTTOM</p></div>
+    </div>
+    <div class="heatMap">
+        <img src="../images/Temporary Picture.jpg" width="100%" height="250px"/>
+    </div>
     <div class="twitter">
         <a class="twitter-timeline" data-width="400" data-height="400" href="https://twitter.com/FortWayneTrails">Tweets by @FortWayneTrails</a>
         <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
