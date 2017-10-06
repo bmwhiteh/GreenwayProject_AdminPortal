9c1764ca04fd5dff019529d9d3f0dc729587fa1b
bmwhiteh <bmwhiteh@gmail.com>
2017-10-05 21:33:40 -0400

    To Run this script: 1. Open XAMPP 2. Start the Server and MySQL 3. Open the Admin for MySQL (should open phpMyAdmin interface) 4. Click SQL 5. copy paste this code into the text box and click go. 6. Database and all tables should be created.

---
+++ sqlFiles/createDatabaseTables.sql
@@ -0,0 +1,107 @@
+/*pushNotifications_ReceivedBy has foreign keys in Users and pushNotifications*/
+/*employees has foreign key in securityLevels*/
+/*maintenanceTickets has foreign key in Users*/
+DROP TABLE IF EXISTS Viridian_Admin_Portal.pushNotifications_ReceivedBy;
+DROP TABLE IF EXISTS Viridian_Admin_Portal.employees;
+DROP TABLE IF EXISTS Viridian_Admin_Portal.maintenanceTickets;
+
+DROP TABLE IF EXISTS Viridian_Admin_Portal.Users;
+DROP TABLE IF EXISTS Viridian_Admin_Portal.pushNotifications;
+DROP TABLE IF EXISTS Viridian_Admin_Portal.securityLevels;
+
+
+DROP DATABASE IF EXISTS Viridian_Admin_Portal;
+
+/*Create the database for Viridian Admin Portal*/
+CREATE DATABASE IF NOT EXISTS Viridian_Admin_Portal;
+
+/*Create Users Table*/
+CREATE TABLE  IF NOT EXISTS Users (
+    intUserId int(5) NOT NULL,
+    strUsername varchar(255), 
+    strEncryptedPassword varchar(255),
+    strEmailAddress varchar(255),
+    strFirstName varchar(255),
+    strLastName varchar(255),
+    intAge int(2), /*2 Digits for Age*/
+    intWeight int(3), /*3 Digits for Weight (whole numbers only)*/
+    intHeight int(2), /*2 Digits for Height (must be saved in inches)*/
+    strGender varchar(255),
+    intActivityTrackedId int(5), /*Running Total of how many activities they've tracked*/
+    intTicketsSubmittedId int(5), /*Running Total of how many tickets they've submitted*/
+    bitSendPictures bit,
+    bitReceiveNotifications bit,
+    intZipCode int(5),
+    dtStartDate date,
+     PRIMARY KEY (intUserId)
+);
+
+/*Create Push Notifications Table*/
+CREATE TABLE  IF NOT EXISTS pushNotifications(
+	intNotificationId int(5) NOT NULL,
+    strNotificationType varchar(255),
+    strNotificationContent varchar(255),
+    dtSentToUsers date,
+    dtReceivedFromAPI date,
+    intUsersSentTo int,
+    strJSONMessage varchar(1000),
+    PRIMARY KEY (intNotificationId)
+
+);
+
+/*Create Table to see what notifications were sent to what users*/
+CREATE TABLE  IF NOT EXISTS pushNotifications_ReceivedBy(
+	intIndex int(5) NOT NULL,    
+    intNotificationId int(5),
+    intUserId int(5),
+    PRIMARY KEY (intIndex),
+    FOREIGN KEY (intUserId) REFERENCES Users(intUserId),
+    FOREIGN KEY (intNotificationId) REFERENCES pushNotifications(intNotificationId)
+);
+
+/*Create security Level Table*/
+CREATE TABLE IF NOT EXISTS securityLevels(
+	intSecurityLevelId int(5) NOT NULL,
+    strLevelDescription varchar(255),
+    strSecurityTitle varchar(255),
+    intEmployeeAssigned int(5),
+    bitAssignTickets bit,
+    bitCloseTickets bit,
+    bitCreateTickets bit,
+    PRIMARY KEY (intSecurityLevelId)
+);
+
+/*Create Employees Table*/
+CREATE TABLE IF NOT EXISTS employees (
+	intEmployeeId int(5) NOT NULL,
+    strFirstName varchar(255),
+    strLastName varchar(255),
+    strUsername varchar(255),
+    strEncryptedPassword varchar(255),
+    intSecurityLevel int(5), /*level descriptions listed in dbo.security_levels*/
+    strEmailAddress varchar(255),
+    PRIMARY KEY (intEmployeeId),
+    FOREIGN KEY (intSecurityLevel) REFERENCES securityLevels(intSecurityLevelId)
+);
+
+
+
+
+/*Create Tickets Table*/
+CREATE TABLE IF NOT EXISTS maintenanceTickets (
+	intTicketId int NOT NULL,
+    strTitle varchar(255),
+    strType varchar(255),
+    strDescription varchar(255),
+    dtSUbmitted date,
+    dtClosed date,
+    strImageFilePath varchar(255),
+    bitUrgent bit,
+    intUserId int,
+    intEmployeeAssigned int,
+    PRIMARY KEY (intTicketId),
+    FOREIGN KEY (intUserId) REFERENCES Users(intUserId),
+    FOREIGN KEY (intEmployeeAssigned) REFERENCES employees(intEmployeeId)
+
+);
+
