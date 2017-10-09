2a8915af5360a3e47a2ac94a27565fa56274c8b2
Andrea Moorman <andreamoorman26@gmail.com>
2017-10-08 22:54:53 -0400

    Push notification page formatting

--- htmlPages/notifications.php
+++ htmlPages/notifications.php
@@ -61,7 +61,7 @@
             text-align: left;
             padding: 8px;
         }
-        tr:hover{
+        tr:hover td{
             background-color: #77be74;
         }
 
@@ -70,6 +70,44 @@
         }
         tr:nth-child(odd){
             background-color: #aaaaaa;
+        }
+        /* The Modal (background) */
+        .modal {
+            display: none; /* Hidden by default */
+            position: fixed; /* Stay in place */
+            z-index: 1; /* Sit on top */
+            padding-top: 300px; /* Location of the box */
+            left: 0;
+            top: 0;
+            width: 100%; /* Full width */
+            height: 100%; /* Full height */
+            overflow: auto; /* Enable scroll if needed */
+            background-color: rgb(0,0,0); /* Fallback color */
+            background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+        }
+
+        /* Modal Content */
+        .modal-content {
+            background-color: #fefefe;
+            margin: auto;
+            padding: 20px;
+            border: 1px solid #888;
+            width: 50%;
+        }
+
+        /* The Close Button */
+        .close {
+            color: #aaaaaa;
+            float: right;
+            font-size: 28px;
+            font-weight: bold;
+        }
+
+        .close:hover,
+        .close:focus {
+            color: #000;
+            text-decoration: none;
+            cursor: pointer;
         }
     </style>
 
@@ -88,8 +126,64 @@
     <div class="recentNotifications">
         <div class="sendNotification">
         <h2>Recent Notifications Sent</h2>
-        <button type="button" >Send New Notification</button>
+        <button id="myBtn" type="button" >Send New Notification</button>
         </div>
+        <!-- The Modal -->
+        <div id="myModal" class="modal">
+
+            <!-- Modal content -->
+            <div class="modal-content">
+                <span class="close">&times;</span>
+                <h2>Send New Push Notification</h2>
+                <form action="/sendPushNotification.php">
+                    <label for="content">Message</label>
+                    <input type="text" id="content" name="content" placeholder="Message..">
+
+
+
+
+                    <label for="dtSend">Send Date</label>
+                    <input type="date" id="dtSend" name="dtSend">
+
+                    <label for="strNotificationType">Type</label>
+                    <select id="strNotificationType" name="strNotificationType">
+                        <option value="event">Local Event</option>
+                        <option value="closure">Trail Closure</option>
+                    </select>
+
+                    <input type="submit" value="Submit">
+                </form>
+            </div>
+
+        </div>
+
+        <script>
+            // Get the modal
+            var modal = document.getElementById('myModal');
+
+            // Get the button that opens the modal
+            var btn = document.getElementById("myBtn");
+
+            // Get the <span> element that closes the modal
+            var span = document.getElementsByClassName("close")[0];
+
+            // When the user clicks the button, open the modal
+            btn.onclick = function() {
+                modal.style.display = "block";
+            }
+
+            // When the user clicks on <span> (x), close the modal
+            span.onclick = function() {
+                modal.style.display = "none";
+            }
+
+            // When the user clicks anywhere outside of the modal, close it
+            window.onclick = function(event) {
+                if (event.target == modal) {
+                    modal.style.display = "none";
+                }
+            }
+        </script>
         <table>
             <tr>
                 <th>Date Sent</th>
