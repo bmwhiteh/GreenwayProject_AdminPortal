b8c8b3b8424da950a7dc4010ec14bc090725431b
dmcshane01 <ddely@live.com>
2017-10-15 16:27:56 -0400

    Added scheduled tasks to tasks page and added tabs

--- htmlPages/tasks.php
+++ htmlPages/tasks.php
@@ -9,6 +9,10 @@
 <?php include "navBar.html"; ?>
 <div class="contentBox">
 
+    <div class="tabs">
+        <button id="showScheduledTasks">Scheduled</button>
+        <button id="showCompletedTassks">Completed</button>
+    </div>
     <div id="taskTableWrapper">
         <table id="completedTaskTable">
         <tr class="tableHeader">
@@ -16,10 +20,43 @@
             <th>Task</th>
             <th>Date</th>
         </tr>
-            <tr>
+            <tr class="task">
                 <td>Admin</td>
                 <td>Completed ticket</td>
                 <td>Date</td>
+            </tr>
+            <tr class="task">
+                <td>User</td>
+                <td>Submitted ticket</td>
+                <td>Date</td>
+            </tr>
+            <tr class="task">
+                <td>User</td>
+                <td>Submitted ticket</td>
+                <td>Date</td>
+            </tr>
+            <tr class="task">
+                <td>User</td>
+                <td>Submitted ticket</td>
+                <td>Date</td>
+            </tr>
+            <tr class="task">
+                <td>User</td>
+                <td>Submitted ticket</td>
+                <td>Date</td>
+            </tr>
+        </table>
+
+        <table id="scheduledTaskTable">
+            <tr class="tableHeader">
+                <th>User</th>
+                <th>Task</th>
+                <th>Date</th>
+            </tr>
+            <tr class="task">
+                <td>Admin</td>
+                <td>Generate weekly report</td>
+                <td>15 Oct 2017 00:00</td>
             </tr>
         </table>
     </div>
