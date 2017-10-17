4f5889d90c931471f4ed5257313717f1533a7414
dmcshane01 <ddely@live.com>
2017-10-17 19:35:06 -0400

    Made user profile page

--- User_Accounts/profile.php
+++ User_Accounts/profile.php
@@ -6,13 +6,35 @@
         .contentBox{
             background-color: #8c8c8c;
             margin: 0px 100px 50px 100px;
-            height: 500px;
+            height: 320px;
+            padding:10px;
         }
 
+        h1 {
+            color: #000;
+            font-size: 24px;
+            font-weight: 400;
+            text-align: center;
+            margin-top: 20px;
+        }
+
+        form{
+            text-align: center;
+            display:block;
+            padding: 14px 20px;
+            border:1px solid black;
+            border-radius:20px;
+            padding-left:20px;
+            background-color:#f2f2f2;
+        }
         body {
             background-color: #1B371A ;
 
         }
+        input{
+            width:30%;
+        }
+
     </style>
 </head>
 <body>
@@ -26,9 +48,21 @@
     ?>
 
     <?php include "../Dashboard_Pages/navBar.html"; ?>
+    <h1 style="margin-bottom: 30px; margin-top:0px; color: white; vertical-aligh:middle; text-align: center;">User Profile</h1>
 
     <div class="contentBox">
-        CONTENT HERE
+        <form style="border:1px solid black" style="padding:2px">
+            <h2>First name:</h2>
+            <input type="text" placeholder="Enter First Name" name="firstName"><br>
+            <h2>Last name:</h2>
+            <input type="test" placeholder="Enter Last Name" name="lastName"><br>
+            <h2>Email:</h2>
+            <input type="test" placeholder="Enter email" name="email"><br>
+
+            <button type="button">Change password</button>
+            <button type="submit">Submit changes</button>
+
+        </form>
     </div>
 
 </body>
