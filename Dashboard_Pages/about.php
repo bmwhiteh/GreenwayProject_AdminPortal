fc51cf7f3f6c8c014f319e864ade2751194eb5ea
Andrea Moorman <andreamoorman26@gmail.com>
2017-10-18 00:24:59 -0400

    about/credits page

--- Dashboard_Pages/about.php
+++ Dashboard_Pages/about.php
@@ -4,45 +4,64 @@
 <head>
     <!--The title of the login page -->
     <title>About</title>
+    <style>
+        body{
+            width: 100%;
+            overflow: hidden;
+            background-color: #1B371A ;
+        }
 
-    <!--Links the CSS stylesheet to the login.html page -->
-    <link rel="stylesheet" type="text/css" href="/GreenwayProject_AdminPortal/css/styles.css"/>
+        .contentBox{
+            background-color: #8c8c8c;
+            margin: 0px 100px 10px 100px;
+            display: -webkit-flex;
+            display: flex;
+            height:470px;
+        }
+        .viridianTeam{
+            height: 30%;
+            background-color: #448b41;
+        }
+
+        .teamTwo{
+            height: 30%;
+            margin-top: 2%;
+            background-color: #55ad52;
+        }
+
+        .teamThree{
+            height: 30%;
+            margin-top: 2%;
+            background-color: #77be74;
+        }
+        .teamBoxes{
+            -webkit-flex: 1;
+            -ms-flex: 1;
+            flex: 1;
+            margin: 1% 1.5% 3% 3%
+        }
+    </style>
+
 </head>
 
-<script>
-    function myFunction() {
-        window.location.href = "/Login_System/login.html"
-    }
-</script>
-
 <body>
-<div class="container">
-    <div class="logo">
-        <img src="../images/ViridianBanner.png" width="100%" height="100px"/>
+
+<?php include "../Dashboard_Pages/navBar.html"; ?>
+<div class="contentBox">
+    <div class="teamBoxes">
+        <div class="viridianTeam">
+            <h2><u>Viridian</u></h2>
+            <p>Insert Info on Viridian Team here.</p>
+        </div>
+        <div class="teamTwo">
+            <h2><u>Team Two</u></h2>
+            <p>Insert Info on Team Two here.</p>
+        </div>
+        <div class="teamThree">
+            <h2><u>Team Three</u></h2>
+            <p>Insert Info on Team Three here.</p>
+        </div>
     </div>
-</div>
-
-<div class="about">
-    <h2>Welcome!</h2>
-    <h3>We bet you're wondering what this page is...</h3>
-    <p>This is the City of Fort Wayne Trail Network Admin Portal.
-        This project has currently been a three year senior capstone project at IPFW,
-        with each team expanding upon the previous team's successes.
-        Currently, we are in Year Three of the Project.  This website was created as
-        a way to display information to the city about the River Greenway.
-        It provides information on what sections of the trail are most used, as
-        well as what types of activities people are using the trails for.  It also
-        has an advanced ticket tracking system that allows the City of Fort Wayne to
-        organize and complete maintenance tasks along the trail system in a quick
-        and timely fashion.
-    </p>
-    <h3>Cool! How can I participate?</h3>
-    <p>Unfortunately, this website can only be accessed by the City of Fort Wayne.
-        However, if you would like to support our project, download the associated app,
-        <b>TrailLinxs</b>.  <b>TrailLinxs</b> is a fitness and activity tracker specific to the River Greenway
-        trail system. And the best part?  &nbsp &nbsp  <i><b><u> It's completely free!</u></b></i> </p>
-
-    <button type="returnToLogin" onClick="myFunction() "><b>Return to Login</b></button>
 </div>
 
 </body>
